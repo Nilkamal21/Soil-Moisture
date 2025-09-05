fa04f02e0dcaba183e52658f82edd983378449a9
Nilkamal21 <nilkamaladhikari2005@gmail.com>
2025-09-05 11:27:49 +0530

    Update app.py

--- scripts/app.py
+++ scripts/app.py
@@ -317,4 +317,5 @@ async def get_audio(filename: str):
 
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("scripts.app:app", host="0.0.0.0", port=8000, reload=True)
+
